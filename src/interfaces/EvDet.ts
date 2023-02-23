d91d443cb70065affba865e56ac699d35b992bc7
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-23 14:35:53 +0100

    created interface for fetched objs

---
+++ src/interfaces/EvDet.ts
@@ -0,0 +1,10 @@
+export interface EvDet {
+  id: number;
+  featured: boolean;
+  title: string;
+  url: string;
+  imageUrl: string;
+  newsSite?: string;
+  summary?: any;
+  publishedAt: string;
+}
