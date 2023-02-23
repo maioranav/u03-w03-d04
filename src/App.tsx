c8ad8c40747511e1c11221fd55377b00bbf0e129
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-23 14:36:03 +0100

    implemented elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "react-bootstrap";
+import "./App.scss";
+import { CustHeader } from "./components/CustHeader";
+import { EventsList } from "./components/EventsList";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <CustHeader />
+        <EventsList />
+      </Container>
     </div>
   );
 }
